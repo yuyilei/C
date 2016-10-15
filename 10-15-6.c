7de2ca1d93953b8c46156d3ad8307091be80e3d9
malingshuseren <yyl1352091742@gmail.com>
2016-10-15 22:08:55 +0800

    helloworld

--- 10-15-6.c
+++ 10-15-6.c
@@ -1,10 +1,15 @@
 #include<stdio.h>
-int cube(int x){
-  return x*x*x;
+int sump (int n){
+  int i,sum;
+  for(i=1;i<=n;i++){
+    sum+=i;
+  }
+  return sum;
 }
  int main(){
    int no;
    printf("请输入一个整数：\n");
    scanf("%d",&no);
-   printf("%d的立方是%d",no,cube(no));
+   printf("%d",sump(no));
    return 0;
+ }

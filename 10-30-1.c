7d6b5d630e2a0f9e7c9fed90417c350f7bd3ceb0
= <=>
2016-10-30 11:46:00 +0800

    helloworld

---
+++ 10-30-1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+printf("I am so sad ,and I want to die .I hate myself now !!!!!1Why am I so sad? Why am I so lonely?\n");
+printf("Even though,I should work harder to build myself stronger.I should keep smiling everyday .So I am  ubuntu now!!!!\n ");
+return 0;
+}

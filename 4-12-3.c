4a21017f4c0812dc94733e7ab007f8091093b8cf
yuyilei <yyl1352091742@gmail.com>
2017-04-18 10:51:38 +0800

    命令行参数9.(7)

--- 4-12-3.c
+++ 4-12-3.c
@@ -13,20 +13,19 @@ int main (int argc , char *argv[]) {
     }
     k = GetMonth(argv[1]) ;
     if ( k == -1 ){
-            printf("erroe!!!") ;
+            printf("error!!!") ;
             exit(0) ;
         }
     printf("%s\n",month[k-1]) ;
     return 0 ;
 }
-
 int GetMonth(char *str) {
     int k, i = 0; 
     while ( str[i] != 0 ){
         if ( str[i] < '0' || str[i] > '9') {
             return (-1) ;
         }
-        else i++ ;
+        i++ ;
     }
     k = atoi(str) ; // 原来可以这样233
     if ( k < 0 || k > 12 ){

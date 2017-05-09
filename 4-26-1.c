6924212988b568451c800542fe2bec4aa00c096e
yuyilei <yyl1352091742@gmail.com>
2017-05-09 17:00:35 +0800

    (1)

--- 4-26-1.c
+++ 4-26-1.c
@@ -1,7 +1,29 @@
 #include<stdio.h>
 #include<string.h>
 int main() {
-    char a[200] ;
-    int i , len , b[20][20] ; 
-
+     short i , j , a[i] , max = 0 , min = 0  ; 
+     unsigned short int b[4] , maxn, minn , temp  ; 
+     for ( i = 0 ; i < 4 ; i++ ){
+        scanf("%hd",&a[i]) ; 
+        b[i] = (unsigned short int)a[i] ; 
+     }
+    maxn = b[0] ; 
+    minn = b[0] ; 
+    for ( i = 0 ; i < 3 ; i++ ){
+        for ( j = 0 ; j + i < 3 ; j++ ){
+            if ( maxn < b[i] ){
+                maxn = b[i] ; 
+                max = i ; 
+            }
+            if ( min > b[i] ){
+                minn = b[i] ;
+                min = i ; 
+            }
+        }
+    }
+    printf("最大无符号数:%u  对应的输入值: %hd\n",a[max],b[max]) ; 
+    printf("最小无符号数:%u  对应的输入值: %hd\n",a[min],b[min]) ; 
+    printf("两数之差：%u\n",a[max]-a[min]) ; 
+    return 0 ; 
 }
+

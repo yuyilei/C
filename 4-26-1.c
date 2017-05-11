02ea05d30e780fb0ec51bc4ed8bf57d9103401f0
yuyilei <yyl1352091742@gmail.com>
2017-05-11 14:36:43 +0800

    (1)

--- 4-26-1.c
+++ 4-26-1.c
@@ -1,24 +1,22 @@
 #include<stdio.h>
 #include<string.h>
 int main() {
-     short i , j , a[i] , max = 0 , min = 0  ; 
-     unsigned short int b[4] , maxn, minn , temp  ; 
+     short i , j , a[4] , max = 0 , min = 0  ; 
+     unsigned short int b[4] , maxn, minn  ; 
      for ( i = 0 ; i < 4 ; i++ ){
         scanf("%hd",&a[i]) ; 
         b[i] = (unsigned short int)a[i] ; 
      }
     maxn = b[0] ; 
     minn = b[0] ; 
-    for ( i = 0 ; i < 3 ; i++ ){
-        for ( j = 0 ; j + i < 3 ; j++ ){
-            if ( maxn < b[i] ){
-                maxn = b[i] ; 
-                max = i ; 
-            }
-            if ( min > b[i] ){
-                minn = b[i] ;
-                min = i ; 
-            }
+    for ( i = 1 ; i < 4 ; i++ ){
+        if ( maxn < b[i] ){
+            maxn = b[i] ; 
+            max = i ; 
+        }
+        if ( minn > b[i] ){
+            minn = b[i] ;
+            min = i ; 
         }
     }
     printf("最大无符号数:%u  对应的输入值: %hd\n",a[max],b[max]) ; 

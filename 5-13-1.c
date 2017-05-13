ee4df995eb4436e784408fa23e3812d444e54099
yuyilei <yyl1352091742@gmail.com>
2017-05-14 00:20:53 +0800

    递归字符串倒叙

--- 5-13-1.c
+++ 5-13-1.c
@@ -1,18 +1,16 @@
 #include<stdio.h>
 #include<string.h>
-void reverse( char a[] ){
-    if ( strlen(a) == 1 ){
-        printf("%c",a[0]) ; 
+void reverse( char *a ){
+    if ( *a == '\0' ){
         return  ; 
     }
     else {
-        char temp[5] ; 
-        strncpy(temp,a,strlen(a)-1) ; 
-        printf("%c",a[strlen(a)-1]) ; 
-        reverse(temp) ; 
+        reverse(a+1) ; 
+        printf("%c",*a) ; 
     }
 }
 int main() {
-    char a[5] = { '1' ,'2' , '3', '4'} ; 
+    char a[40] ;   
+    gets(a) ; 
     reverse(a) ; 
 }

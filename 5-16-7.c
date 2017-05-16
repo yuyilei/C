8cc7afb43e6f9dc25c8678d6dfc0ae5c26f7a2ac
yuyilei <yyl1352091742@gmail.com>
2017-05-16 17:15:21 +0800

    5.(5)

--- 5-16-7.c
+++ 5-16-7.c
@@ -2,7 +2,7 @@
 int main() {
     int year , mon , flag = 0 , month[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31} ; 
     scanf("%d%d",&year,&mon) ; 
-    if ( (( year % 4 ) == 0 && (year % 100) != 0) || (( year % 400 ) == 0 )){
+    if ( (( year % 4 ) == 0 && (year % 100) != 0) || (( year % 400 ) == 0 ) ){
         flag = 1 ; 
     }
     if ((mon==2) && (flag==1)){

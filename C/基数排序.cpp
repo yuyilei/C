b6f55569c3ad3c98beb16fa73dca02973f0ef06d
yuyilei <yyl1352091742@gmail.com>
2017-09-28 22:43:11 +0800

    homework

---
+++ C/基数排序.cpp
@@ -0,0 +1,99 @@
+#include<iostream>
+#include<cstdio>
+using namespace std ; 
+struct node { 
+    struct node *next ; 
+    int data ; 
+    node() : data(0), next(NULL) {}
+} ; 
+int num , bit = 4 ;  
+struct node * InitNode(struct node * head , int & num ) {
+    int n , id , age , i , t1 , t2 ; 
+    struct node *tmp = head ; 
+    cin >> n ; 
+    num = n ; 
+    for ( i = 0 ; i < n ; i++ ) {
+        tmp->next = new node() ; 
+        tmp = tmp->next ; 
+        cin >> tmp->data ; 
+    } 
+    return head ; 
+}
+void show_elem(struct node *head ) {
+    struct node *tmp = head->next ;
+    cout << "Show elem:" << endl ; 
+    while ( tmp ) {
+        cout << tmp->data ; 
+        tmp = tmp->next ; 
+        if ( tmp )
+            cout << " -> " ; 
+    }
+    cout << "\n\n" ; 
+}
+
+struct node * GetFirstNode(struct node  *head) {
+    if ( head->next == NULL )  
+        return NULL;
+    struct node *p ;
+    p = head->next ;
+    head->next = p->next ;
+    return p ;
+}
+
+void AppendNode(struct node  *head,struct node  *tmp) {
+    if ( head == NULL ) 
+        return ;
+    struct node  *p ;
+    p = head ;
+    while ( p->next != NULL ) 
+        p = p->next ;
+    p->next = tmp ;
+    tmp->next = NULL ;
+}
+
+void Total(struct node  *L, struct node  *tmp ) {
+	struct node  *p = L ;
+	while ( p->next != NULL ) 
+		p = p->next ;
+	p->next = tmp->next ;
+}
+
+int GetNum(struct node  *p , int pos ) {
+    int data = p->data , res , i = 0 ;
+    while ( i <= pos ) {
+        res = data % 10 ; 
+        data /= 10 ; 
+        i++ ; 
+    }
+    return res ; 
+} 
+
+void radix_sort(struct node *head) {
+	struct node  *p[10] , *q ;
+	int i , j , k ;
+    for( j = 1 ; j <= bit ; j++ ){
+	    for( i = 0 ; i < 10 ; i++ )
+		    p[i] = new node() ; 
+	    while( head->next!= NULL ) {
+            q = GetFirstNode(head) ;
+            k = GetNum(q,j) ; 
+            AppendNode(p[k],q) ; 
+        }
+        for( i = 0 ; i < 10 ; i++) 
+            Total(head,p[i]) ;
+	}
+	for( i = 0 ; i < 10 ; i++ ) 
+		delete(p[i]);
+}
+
+int main() {
+    struct node *head = new node() , *tmp = new node(); 
+    head = InitNode(head,num) ;
+    cout << endl ; 
+    cout << "Before radix sort    "  ; 
+    show_elem(head) ; 
+    radix_sort(head) ; 
+    cout << "\nAfter radix sort     "  ; 
+    show_elem(head) ; 
+    return 0 ; 
+} 
